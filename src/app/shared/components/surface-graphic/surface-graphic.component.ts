27bb9b899a4fdc85dcb98c5ab75158d3815b9756
abinassar <abinassar@gmail.com>
2024-01-03 21:29:10 -0400

    commit before install plotly to angular

---
+++ src/app/shared/components/surface-graphic/surface-graphic.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-surface-graphic',
+  templateUrl: './surface-graphic.component.html',
+  styleUrls: ['./surface-graphic.component.scss'],
+})
+export class SurfaceGraphicComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
